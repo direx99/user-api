2318f97e054dfe9fde1cba41e880cfbbad7651d3
Dinith Hasaranga <101465561+direx99@users.noreply.github.com>
2023-07-08 09:51:56 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv")
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8070;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+mongoose.set("strictQuery", false);
+
+mongoose.connect(URL,{
+   
+    
+   
+    
+     
+});
+
+const connection = mongoose.connection;
+connection.once('open',()=>{
+    console.log("Mongo DB Connected Successfuly done !");
+})
+
+
+
+
+app.listen(PORT,()=>{
+    console.log(`server is up & PORT is "${PORT}`);
+
+})
